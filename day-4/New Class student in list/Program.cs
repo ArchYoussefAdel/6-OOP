61dee4de3f9db132422b007cb9de51da7124273d
Youssef Adel <arch.youssef.adel@gmail.com>
2023-08-13 18:13:21 +0300

    add

---
+++ day-4/New Class student in list/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Student
+{
+    private int[] grades;
+    public string Name { get; set; }
+    public Student()
+    {
+        grades = new int[10];
+    }
+    public int this[int subject]
+    {
+        get
+        {
+            if (subject >= 1 && subject <= 10)
+                return grades[subject - 1];
+
+            return 0;
+        }
+        set
+        {
+            if (subject >= 1 && subject <= 10)
+                grades[subject - 1] = value;
+        }
+    }
+    public int Total()
+    {
+        int total = 0;
+        foreach (var grade in grades)
+        {
+            total += grade;
+        }
+        return total;
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter the number of students: ");
+        int numberOfStudents = int.Parse(Console.ReadLine());
+
+        List<Student> students = new List<Student>();
+
+        for (int i = 0; i < numberOfStudents; i++)
+        {
+            Console.Write($"Enter the name of student {i + 1}: ");
+            string name = Console.ReadLine();
+
+            Student student = new Student();
+            student.Name = name;
+
+            for (int j = 1; j <= 10; j++)
+            {
+                Console.Write($"Enter the grade for subject {j} for {name}: ");
+                int grade = int.Parse(Console.ReadLine());
+                student[j] = grade;
+            }
+            students.Add(student);
+        }
+        Console.WriteLine("\n--- Student Details ---");
+        foreach (var student in students)
+        {
+            Console.WriteLine($"Total marks for {student.Name}: {student.Total()}");
+        }
+    }
+}
