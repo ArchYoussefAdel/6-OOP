b3f1c67d01d35c5bdbfbe34e72802ccea10764ec
Youssef Adel <arch.youssef.adel@gmail.com>
2023-08-15 08:03:22 +0300

    Updated Version
    some updates to improve the main code

---
+++ day-5/Colors with indexers and try and catch/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace ColorArray
+{
+    class ColorArray
+    {
+        private string[] colors;
+
+        public ColorArray()
+        {
+            colors = new string[5] { "Red", "Blue", "Green", "Yellow", "Orange" };
+        }
+
+        public string this[int index]
+        {
+            get
+            {
+                return colors[index];
+            }
+            set
+            {
+                colors[index] = value;
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ColorArray colorArray = new ColorArray();
+            Console.WriteLine("Enter a color index (1-5): ");
+            try
+            {
+                int index = int.Parse(Console.ReadLine()) - 1;
+                Console.WriteLine($"The color at index {index + 1} is {colorArray[index]}");
+            }
+            catch (IndexOutOfRangeException ioore)
+            {
+                Console.WriteLine("Invalid index. Please enter a number between 1 and 5.");
+                Console.WriteLine(ioore.Message);
+            }
+            catch (FormatException FE)
+            {
+                Console.WriteLine("Invalid input. Please enter a number between 1 and 5 because there is only 5 numbers.");
+                Console.WriteLine(FE.Message);
+            }
+        }
+    }
+}
---
+++ day-5/Exception Handling/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exception_Handling
+{
+    class Test
+    {
+        static int I;
+        private static void Welcome()
+        { Console.WriteLine("Welcome to Our Test Class"); }
+        private static void GoodBye()
+        { Console.WriteLine("This is the end of our Class"); }
+        public static void PrintMessage()
+        {
+            Console.WriteLine("This Message from Test Class");
+            Welcome();
+        }
+        public static void DivideNumbers()
+        {
+            try
+            {
+                Console.WriteLine("Enter first Number");
+                int I = int.Parse(Console.ReadLine());
+                Console.WriteLine("Enter second number");
+                int j = int.Parse(Console.ReadLine());
+                int k = I / j;
+                Console.WriteLine($"The result of dividing {I} by {j} is {k}");
+            }
+            catch (OverflowException e)
+            {
+                Console.WriteLine(e);
+            }
+            catch (DivideByZeroException e)
+            {
+                Console.WriteLine(e);
+            }
+        }
+        public static void Main()
+        {
+            try
+            {
+                Console.Write("Enter an integer: ");
+                I = int.Parse(Console.ReadLine());
+                PrintMessage();
+            }
+            catch (FormatException FE)
+            {
+                Console.WriteLine("Invalid input. Please enter an integer.");
+                Console.WriteLine(FE);
+            }
+            finally
+            {
+                GoodBye();
+            }
+
+            DivideNumbers();
+
+        }
+    }
+
+}
